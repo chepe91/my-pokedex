939b76189bba1a49ee97e80782b48a07a25d7d20
Jose Ontiveros <joseontiverosn@gmail.com>
2022-01-14 23:59:57 -0700

    pokedex fixes

--- src/pages/Pokedex.tsx
+++ src/pages/Pokedex.tsx
@@ -2,18 +2,28 @@ import React, { useEffect, useState } from "react";
 import { PokemonList } from "../components/PokemonList";
 //https://pokeapi.co/api/v2/pokemon/
 
+export type PokemonData = {
+  id:number,
+  format_id:string,
+  name: string,
+  types: any[]
+}
+
 export const Pokedex = () => {
-  const [pokemonList, setPokemonList] = useState([]);
+  const [pokemonList, setPokemonList] = useState<any[]>([]);
   const [pokemonCount, setPokemonCount] = useState(0);
   const [loadMoreUrl, setLoadMoreUrl] = useState("");
+
 
   useEffect(() => {
     fetch("https://pokeapi.co/api/v2/pokemon/")
       .then((res) => res.json())
       .then((data) => {
         setPokemonCount(data.count);
-        setPokemonList(data.results);
         setLoadMoreUrl(data.next);
+        Promise.all(data.results.map((pokemon: any) => fetchPokemonData(pokemon))).then(() => {
+      
+        });
       });
   }, []);
 
@@ -22,13 +32,30 @@ export const Pokedex = () => {
       fetch(loadMoreUrl)
         .then((res) => res.json())
         .then((data) => {
-          setPokemonList((prevState) => {
-            return prevState.concat(data.results);
-          });
+          
           setLoadMoreUrl(data.next);
+          Promise.all(data.results.map((pokemon: any) => fetchPokemonData(pokemon))).then(() => {
+            
+          });
         });
     }
   };
+
+  async function fetchPokemonData(pokemon: any) {
+    const data = await (await fetch(pokemon.url)).json();
+    const pokemonData: PokemonData = {
+      id: data.id,
+      name: data.name,
+      format_id: String(data.id).padStart(3, '0'),
+      types: data.types
+    };
+
+    console.log(pokemonData);
+    setPokemonList((prevState: PokemonData[]) => {
+      return [...prevState, pokemonData];
+    });
+  }
+
 
   return (
     <>
